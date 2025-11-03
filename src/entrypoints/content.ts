9e5c38e439756cdad0e17ce6479038474e68a771
zenji <canhtoannguyen60@gmail.com>
2025-11-03 22:46:06 +0700

    patch: remove content script

--- src/entrypoints/content.ts
+++
@@ -1,8 +0,0 @@
-import { defineContentScript } from '#imports'; 
-
-export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
-  },
-});
