ed73a71fcee12afda43d83d955856797351918c2
zenji <canhtoannguyen60@gmail.com>
2025-11-26 17:13:21 +0700

    fix: lint

--- src/hooks/use-translation.ts
+++ src/hooks/use-translation.ts
@@ -1,5 +1,5 @@
 import { useAppSelector } from '@/store/hook';
-import i18n from '@/lib/i18n';
+import i18n from '@/lib/i18n/index';
 import { Namespace } from '@/lib/i18n/types';
 
 export const useTranslation = (namespace: Namespace = 'common') => {
